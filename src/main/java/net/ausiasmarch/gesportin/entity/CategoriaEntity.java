ec1496fd7907f3c2ec11d35e778f0063632005bd
Alan Alcañiz <alanalcaniz24@gmail.com>
2026-01-15 11:41:53 +0100

    manytoone

--- src/main/java/net/ausiasmarch/gesportin/entity/CategoriaEntity.java
+++ src/main/java/net/ausiasmarch/gesportin/entity/CategoriaEntity.java
@@ -1,10 +1,12 @@
 package net.ausiasmarch.gesportin.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -18,7 +20,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CategoriaEntity {
-    
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
@@ -28,6 +30,7 @@ public class CategoriaEntity {
     private String nombre;
 
     @NotNull
-    @Column(name = "id_temporada")
-    private Long idTemporada;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_temporada")
+    private TemporadaEntity temporada;
 }
--- src/main/java/net/ausiasmarch/gesportin/entity/TipoarticuloEntity.java
+++ src/main/java/net/ausiasmarch/gesportin/entity/TipoarticuloEntity.java
@@ -2,9 +2,12 @@ package net.ausiasmarch.gesportin.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -28,6 +31,7 @@ public class TipoarticuloEntity {
     private String descripcion;
 
     @NotNull
-    @Column(name = "id_club", nullable = false)
-    private Long idClub;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_club")
+    private ClubEntity club;
 }
